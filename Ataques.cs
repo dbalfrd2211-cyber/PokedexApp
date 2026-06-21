aacd488bbbae721d2c50f07c128730923f27a6c3
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-21 01:07:50 -0600

    ataq

---
+++ Ataques.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class Ataques
+    {
+        
+        public int IdAtaque { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public int Daño { get; set; }
+        public int IdEfecto { get; set; }
+        public Ataques(int idAtaque, string nombre, string tipo, int daño, int idEfecto)
+        {
+            IdAtaque = idAtaque;
+            Nombre = nombre;
+            Tipo = tipo;
+            Daño = daño;
+            IdEfecto = idEfecto;
+        }
+
+    }
+}
