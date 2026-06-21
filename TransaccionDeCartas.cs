4a3245770b34c7123d9f11a4509c5a5a6f710cb5
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-21 01:26:42 -0600

    intercambio

---
+++ TransaccionDeCartas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class TransaccionDeCartas
+    {
+        
+        public int IdTransaccion { get; set; }
+        public int IdUsuarioEmisor { get; set; }
+        public int IdUsuarioReceptor { get; set; }
+        public int IdPokemonEntregado { get; set; }
+        public int IdPokemonRecibido { get; set; }
+
+        public TransaccionDeCartas(int idTransaccion, int idUsuarioEmisor, int idUsuarioReceptor, int idPokemonEntregado, int idPokemonRecibido)
+        {
+            IdTransaccion = idTransaccion;
+            IdUsuarioEmisor = idUsuarioEmisor;
+            IdUsuarioReceptor = idUsuarioReceptor;
+            IdPokemonEntregado = idPokemonEntregado;
+            IdPokemonRecibido = idPokemonRecibido;
+        }
+
+    }
+}
