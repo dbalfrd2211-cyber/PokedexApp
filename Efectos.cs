848dfe26394cdbe25f87d20ad1684754f3229f95
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-21 01:03:43 -0600

    efectos

---
+++ Efectos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class Efectos
+    {
+     
+
+        public  int IdEfecto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public Efectos(int idEfecto, string nombre, string descripcion)
+        {
+            IdEfecto = idEfecto;
+            Nombre = nombre;
+            Descripcion = descripcion;
+        }
+    }
+}
