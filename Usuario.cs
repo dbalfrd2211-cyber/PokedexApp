1a7582aed14faf109830e087e2b0b0561d5eec2b
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-20 20:48:34 -0600

    ususuario

---
+++ Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class Usuario
+    {
+         public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+         public bool Espublico { get; set; }
+
+        public Usuario(int idUsuario, string nombreUsuario, string contraseña, bool espublico)
+        {
+            IdUsuario = idUsuario;
+            NombreUsuario = nombreUsuario;
+            Contraseña = contraseña;
+            Espublico = espublico;
+        }
+    }
+}
