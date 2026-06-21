76d78c4970c5f2fe31885f190b4fd2d4001968f4
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-21 01:00:56 -0600

    colec

---
+++ ColeccionUsuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class ColeccionUsuario
+    {
+      
+        public int IdUsuario { get; set; }
+        public int IdPokemon { get; set; }
+        public int Cantidades { get; set; }
+
+        public ColeccionUsuario(int idUsuario, int idPokemon, int cantidades)
+        {
+            IdUsuario = idUsuario;
+            IdPokemon = idPokemon;
+            Cantidades = cantidades;
+        }
+
+
+    }
+}
