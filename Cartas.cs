b9a5503d28ea0a6ca83ab7e440970502aa832f6f
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-21 01:12:51 -0600

    cartas

---
+++ Cartas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class Cartas
+    {
+        
+        public int IdCarta { get; set; }
+        public int IdPokemon { get; set; }
+        public int Hp { get; set; }
+        public string Rareza { get; set; }
+        public int NumeroDeColeccion { get; set; }
+
+        public Cartas(int idCarta, int idPokemon, int hp, string rareza, int numeroDeColeccion)
+        {
+            IdCarta = idCarta;
+            IdPokemon = idPokemon;
+            Hp = hp;
+            Rareza = rareza;
+            NumeroDeColeccion = numeroDeColeccion;
+        }
+
+
+    }
+}
