774a44061932d42188f7de235a7d00f8da2bcc82
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-20 19:35:20 -0600

    clase region

---
+++ Regiones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class Regiones
+    {
+       
+        public int IdRegion { get; }
+        public string NombreRegion { get; }
+
+        public Regiones(int idRegion, string nombreRegion)
+        {
+            IdRegion = idRegion;
+            NombreRegion = nombreRegion;
+        }
+
+    }
+}
