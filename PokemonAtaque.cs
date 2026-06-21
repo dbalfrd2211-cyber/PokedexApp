cbaa4f0787ecda5f6337ed5f038d68b21160a6e9
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-21 01:10:04 -0600

    poata

---
+++ PokemonAtaque.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class PokemonAtaque
+    {
+        public int IdPokemonAtaque { get; set; }
+        public int IdPokemon { get; set; } 
+        public int IdAtaque { get; set; }
+        public PokemonAtaque(int idPokemonAtaque, int idPokemon, int idAtaque)
+        {
+            IdPokemonAtaque = idPokemonAtaque;
+            IdPokemon = idPokemon;
+            IdAtaque = idAtaque;
+        }   
+
+    }
+}
