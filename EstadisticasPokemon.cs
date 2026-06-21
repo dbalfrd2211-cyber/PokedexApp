ecd7468ac0ac65685266e92fa3e1e185b3e9918d
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-20 20:48:34 -0600

    estadisticas

---
+++ EstadisticasPokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class EstadisticasPokemon
+    {
+        public int IdPokemon { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+        public int HPBase { get; set; }
+
+        public EstadisticasPokemon(int idPokemon, double altura, double peso, int hPBase)
+        {
+            IdPokemon = idPokemon;
+            Altura = altura;
+            Peso = peso;
+            HPBase = hPBase;
+        }
+
+
+    }
+}
