ed2fbdaf7835461515728df01e13386cfa122222
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-20 21:36:30 -0600

    part

---
+++ Partida.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp
+{
+    internal class Partida
+    {
+
+        public int IdPartida { get; set; }
+        public int IdJugador1 { get; set; }
+        public int IdJugador2 { get; set; }
+        public int Ganador { get; set; }
+        public DateTime Fecha { get; set; }
+
+      
+    }
+}
