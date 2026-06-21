900c951ddd6c7d762a533b6307ba459609579216
fabiCasanova <A20203250@alumnos.uady.mx>
2026-06-20 19:35:20 -0600

    clase pokemon

--- Pokemon.cs
+++ Pokemon.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace PokedexApp
 {
     internal class Pokemon
-    {
+
+    {  
+        //Nombre, Hp, Tipo, Puntos de ataque, Puntos de defensa, Numero de carta, Descripción. 
+
+        public int Id { get; }
+        public string Nombre { get; }
+        public int Hp { get; }
+        public string Tipo { get; }
+        public int PuntosDeAtaque { get; }
+        public int PuntosDeDefensa { get; }
+        public int NumeroDeCarta { get; }
+        public string Descripción { get; }
+        public Pokemon(int id, string nombre, int hp, string tipo, int puntosDeAtaque, int puntosDeDefensa, int numeroDeCarta, string descripción)
+        {
+            Id = id;
+            Nombre = nombre;
+            Hp = hp;
+            Tipo = tipo;
+            PuntosDeAtaque = puntosDeAtaque;
+            PuntosDeDefensa = puntosDeDefensa;
+            NumeroDeCarta = numeroDeCarta;
+            Descripción = descripción;
+        }
+
+
+
     }
 }
