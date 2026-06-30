a921cd969bd460bd9d34165589a85a1bcf2f283c
JESUS EMMANUEL UCAN CHAN <a19201483@alumnos.uady.mx>
2026-06-29 22:29:38 -0600

    Agregar ventana de intercambio de cartas

---
+++ IntercambioCartas.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace PokedexApp
+{
+    partial class IntercambioCartas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnAgregarU1 = new System.Windows.Forms.Button();
+            this.btnAgregarU2 = new System.Windows.Forms.Button();
+            this.btnRetirarU1 = new System.Windows.Forms.Button();
+            this.btnRetirarU2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(313, 104);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuario 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Colección";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Colección";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(424, 72);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(313, 104);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(15, 248);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(313, 98);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(424, 248);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowHeadersWidth = 51;
+            this.dataGridView4.RowTemplate.Height = 24;
+            this.dataGridView4.Size = new System.Drawing.Size(313, 98);
+            this.dataGridView4.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Cartas a entregar usuario 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(421, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(167, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Cartas a entregar usuario 2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Realizar intercambio";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregarU1
+            // 
+            this.btnAgregarU1.Location = new System.Drawing.Point(15, 183);
+            this.btnAgregarU1.Name = "btnAgregarU1";
+            this.btnAgregarU1.Size = new System.Drawing.Size(173, 29);
+            this.btnAgregarU1.TabIndex = 3;
+            this.btnAgregarU1.Text = "Agregar al intercambio";
+            this.btnAgregarU1.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregarU2
+            // 
+            this.btnAgregarU2.Location = new System.Drawing.Point(424, 182);
+            this.btnAgregarU2.Name = "btnAgregarU2";
+            this.btnAgregarU2.Size = new System.Drawing.Size(173, 29);
+            this.btnAgregarU2.TabIndex = 3;
+            this.btnAgregarU2.Text = "Agregar al intercambio";
+            this.btnAgregarU2.UseVisualStyleBackColor = true;
+            // 
+            // btnRetirarU1
+            // 
+            this.btnRetirarU1.Location = new System.Drawing.Point(15, 351);
+            this.btnRetirarU1.Name = "btnRetirarU1";
+            this.btnRetirarU1.Size = new System.Drawing.Size(173, 29);
+            this.btnRetirarU1.TabIndex = 3;
+            this.btnRetirarU1.Text = "Retirar del intercambio";
+            this.btnRetirarU1.UseVisualStyleBackColor = true;
+            // 
+            // btnRetirarU2
+            // 
+            this.btnRetirarU2.Location = new System.Drawing.Point(424, 350);
+            this.btnRetirarU2.Name = "btnRetirarU2";
+            this.btnRetirarU2.Size = new System.Drawing.Size(173, 29);
+            this.btnRetirarU2.TabIndex = 3;
+            this.btnRetirarU2.Text = "Retirar del intercambio";
+            this.btnRetirarU2.UseVisualStyleBackColor = true;
+            // 
+            // IntercambioCartas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRetirarU2);
+            this.Controls.Add(this.btnRetirarU1);
+            this.Controls.Add(this.btnAgregarU2);
+            this.Controls.Add(this.btnAgregarU1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "IntercambioCartas";
+            this.Text = "IntercambioCartas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAgregarU1;
+        private System.Windows.Forms.Button btnAgregarU2;
+        private System.Windows.Forms.Button btnRetirarU1;
+        private System.Windows.Forms.Button btnRetirarU2;
+    }
+}
