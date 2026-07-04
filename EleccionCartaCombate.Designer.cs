1022c1a6276a75c5f8d16001f7e946330390b9bc
JESUS EMMANUEL UCAN CHAN <a19201483@alumnos.uady.mx>
2026-07-03 22:22:37 -0600

    cclase eleccioncarta

---
+++ EleccionCartaCombate.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace PokedexApp
+{
+    partial class EleccionCartaCombate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserInter1 = new System.Windows.Forms.Label();
+            this.lblUsuario2 = new System.Windows.Forms.Label();
+            this.DGVAgregarU2 = new System.Windows.Forms.DataGridView();
+            this.DGVAgregarU1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVAgregarU2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVAgregarU1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUserInter1
+            // 
+            this.txtUserInter1.AutoSize = true;
+            this.txtUserInter1.Location = new System.Drawing.Point(139, 40);
+            this.txtUserInter1.Name = "txtUserInter1";
+            this.txtUserInter1.Size = new System.Drawing.Size(119, 16);
+            this.txtUserInter1.TabIndex = 7;
+            this.txtUserInter1.Text = "Usuario Logueado";
+            // 
+            // lblUsuario2
+            // 
+            this.lblUsuario2.AutoSize = true;
+            this.lblUsuario2.Location = new System.Drawing.Point(569, 40);
+            this.lblUsuario2.Name = "lblUsuario2";
+            this.lblUsuario2.Size = new System.Drawing.Size(64, 16);
+            this.lblUsuario2.TabIndex = 6;
+            this.lblUsuario2.Text = "Usuario 2";
+            // 
+            // DGVAgregarU2
+            // 
+            this.DGVAgregarU2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVAgregarU2.Location = new System.Drawing.Point(443, 70);
+            this.DGVAgregarU2.MultiSelect = false;
+            this.DGVAgregarU2.Name = "DGVAgregarU2";
+            this.DGVAgregarU2.RowHeadersWidth = 51;
+            this.DGVAgregarU2.RowTemplate.Height = 24;
+            this.DGVAgregarU2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGVAgregarU2.Size = new System.Drawing.Size(333, 159);
+            this.DGVAgregarU2.TabIndex = 8;
+            // 
+            // DGVAgregarU1
+            // 
+            this.DGVAgregarU1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVAgregarU1.Location = new System.Drawing.Point(33, 69);
+            this.DGVAgregarU1.MultiSelect = false;
+            this.DGVAgregarU1.Name = "DGVAgregarU1";
+            this.DGVAgregarU1.RowHeadersWidth = 51;
+            this.DGVAgregarU1.RowTemplate.Height = 24;
+            this.DGVAgregarU1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGVAgregarU1.Size = new System.Drawing.Size(339, 159);
+            this.DGVAgregarU1.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(404, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 433);
+            this.panel1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 267);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 146);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.Location = new System.Drawing.Point(148, 267);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(109, 146);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox3.Location = new System.Drawing.Point(263, 267);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(109, 146);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox4.Location = new System.Drawing.Point(670, 267);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(109, 146);
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox5.Location = new System.Drawing.Point(555, 267);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(109, 146);
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox6.Location = new System.Drawing.Point(440, 267);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(109, 146);
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Cartas a combatir:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(552, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Cartas a combatir:";
+            // 
+            // EleccionCartaCombate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DGVAgregarU2);
+            this.Controls.Add(this.DGVAgregarU1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserInter1);
+            this.Controls.Add(this.lblUsuario2);
+            this.Name = "EleccionCartaCombate";
+            this.Text = "EleccionCartaCombate";
+            ((System.ComponentModel.ISupportInitialize)(this.DGVAgregarU2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVAgregarU1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtUserInter1;
+        private System.Windows.Forms.Label lblUsuario2;
+        private System.Windows.Forms.DataGridView DGVAgregarU2;
+        private System.Windows.Forms.DataGridView DGVAgregarU1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
