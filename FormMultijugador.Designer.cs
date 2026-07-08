fbe8fa1392cafd8a090183eae1b2606a52439183
JESUS EMMANUEL UCAN CHAN <a19201483@alumnos.uady.mx>
2026-07-07 21:01:59 -0600

    mejoras de interfaz

--- FormMultijugador.Designer.cs
+++ FormMultijugador.Designer.cs
@@ -34,13 +34,23 @@
             this.lblUsuarioLogueado = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.cmbUsuario2 = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCombate
             // 
-            this.btnCombate.Location = new System.Drawing.Point(109, 153);
+            this.btnCombate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCombate.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCombate.Location = new System.Drawing.Point(53, 3);
             this.btnCombate.Name = "btnCombate";
-            this.btnCombate.Size = new System.Drawing.Size(225, 153);
+            this.btnCombate.Size = new System.Drawing.Size(401, 363);
             this.btnCombate.TabIndex = 0;
             this.btnCombate.Text = "Batalla 1 vs 1";
             this.btnCombate.UseVisualStyleBackColor = true;
@@ -48,9 +58,11 @@
             // 
             // btnVolverMenu
             // 
-            this.btnVolverMenu.Location = new System.Drawing.Point(291, 345);
+            this.btnVolverMenu.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVolverMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolverMenu.Location = new System.Drawing.Point(368, 522);
             this.btnVolverMenu.Name = "btnVolverMenu";
-            this.btnVolverMenu.Size = new System.Drawing.Size(227, 76);
+            this.btnVolverMenu.Size = new System.Drawing.Size(288, 82);
             this.btnVolverMenu.TabIndex = 2;
             this.btnVolverMenu.Text = "Volver al menu principal";
             this.btnVolverMenu.UseVisualStyleBackColor = true;
@@ -58,9 +70,11 @@
             // 
             // btnIntercambio
             // 
-            this.btnIntercambio.Location = new System.Drawing.Point(457, 153);
+            this.btnIntercambio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnIntercambio.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIntercambio.Location = new System.Drawing.Point(561, 3);
             this.btnIntercambio.Name = "btnIntercambio";
-            this.btnIntercambio.Size = new System.Drawing.Size(225, 153);
+            this.btnIntercambio.Size = new System.Drawing.Size(401, 363);
             this.btnIntercambio.TabIndex = 0;
             this.btnIntercambio.Text = "Intercambio de Cartas";
             this.btnIntercambio.UseVisualStyleBackColor = true;
@@ -68,47 +82,116 @@
             // 
             // lblUsuarioLogueado
             // 
+            this.lblUsuarioLogueado.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblUsuarioLogueado.AutoSize = true;
-            this.lblUsuarioLogueado.Location = new System.Drawing.Point(149, 39);
+            this.lblUsuarioLogueado.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuarioLogueado.Location = new System.Drawing.Point(84, 38);
             this.lblUsuarioLogueado.Name = "lblUsuarioLogueado";
-            this.lblUsuarioLogueado.Size = new System.Drawing.Size(119, 16);
+            this.lblUsuarioLogueado.Size = new System.Drawing.Size(341, 42);
             this.lblUsuarioLogueado.TabIndex = 1;
             this.lblUsuarioLogueado.Text = "Usuario Logueado";
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(464, 39);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.Size = new System.Drawing.Size(187, 42);
             this.label1.TabIndex = 1;
             this.label1.Text = "Usuario 2";
             // 
             // cmbUsuario2
             // 
+            this.cmbUsuario2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cmbUsuario2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbUsuario2.FormattingEnabled = true;
-            this.cmbUsuario2.Location = new System.Drawing.Point(534, 36);
+            this.cmbUsuario2.Location = new System.Drawing.Point(253, 35);
             this.cmbUsuario2.Name = "cmbUsuario2";
-            this.cmbUsuario2.Size = new System.Drawing.Size(121, 24);
+            this.cmbUsuario2.Size = new System.Drawing.Size(197, 50);
             this.cmbUsuario2.TabIndex = 3;
             this.cmbUsuario2.SelectedIndexChanged += new System.EventHandler(this.cmbUsuario2_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnVolverMenu, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1025, 626);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.lblUsuarioLogueado, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1019, 119);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cmbUsuario2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(512, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 113);
+            this.panel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 5;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel3.Controls.Add(this.btnIntercambio, 3, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnCombate, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 128);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1019, 369);
+            this.tableLayoutPanel3.TabIndex = 1;
             // 
             // FormMultijugador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cmbUsuario2);
-            this.Controls.Add(this.btnVolverMenu);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.lblUsuarioLogueado);
-            this.Controls.Add(this.btnIntercambio);
-            this.Controls.Add(this.btnCombate);
+            this.ClientSize = new System.Drawing.Size(1025, 626);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "FormMultijugador";
             this.Text = "FormMultijugador";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FormMultijugador_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -120,5 +203,9 @@
         private System.Windows.Forms.Label lblUsuarioLogueado;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cmbUsuario2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
     }
 }
